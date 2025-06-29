d354fe3c71a326b55770947ce2cc237af98724b6
Guilherme Augusto <guilhermedev23@gmail.com>
2025-06-29 16:36:28 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-
-import {  Text, View } from 'react-native';
-
-
+import { Text, View,StatusBar  } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto"
 export default function App() {
+ const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular
+  })
   return (
-    <View style={{ flex: 1,  alignItems: 'center', justifyContent: 'center', }}>
-      <Text>Home Home</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', 
+     backgroundColor:"#202024",
+    }}
+    >
+       <StatusBar  barStyle="light-content" backgroundColor="transparent"
+       translucent />
+
+       
+      {fontsLoaded ? <Text>Home</Text>:<View />}
     </View>
   );
 }
