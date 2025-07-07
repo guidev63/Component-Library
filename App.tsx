a438020ddbcd8e434e233c4e2ac4bcf6104e1374
Guilherme Augusto <guilhermedev23@gmail.com>
2025-07-06 22:50:39 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -3,36 +3,32 @@ import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from "@expo-google-fonts/roboto"
+} from "@expo-google-fonts/roboto";
 
-import { GluestackUIProvider, Text } from "@gluestack-ui/themed"
+import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
+
+import { config } from "@gluestack-ui/config";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
-    Roboto_400Regular
-  })
+    Roboto_400Regular,
+  });
+
   return (
-    <GluestackUIProvider>
-    <View style={{
-      flex: 1,
-       alignItems: 'center',
-       justifyContent: 'center',
-       backgroundColor: "#202024",
-    }}
-    >
+    <GluestackUIProvider config={config}>
       <StatusBar
-       barStyle="light-content"
+        barStyle="light-content"
         backgroundColor="transparent"
-        translucent 
-        />
-      {
-      fontsLoaded ? 
-      <Text>Home Home Home </Text> 
-      : 
-      <View />
-      }
-    </View>
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bg="$info600">
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <View />
+      )}
     </GluestackUIProvider>
   );
 }
-
