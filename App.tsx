5f6a33b3c71beb7e095c4e432c3b0b806845b8f6
Guilherme Augusto <guilhermedev23@gmail.com>
2025-07-10 16:34:18 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
 
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
-
+import { } from '@screens/SignIn'
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,7 @@
+import { VStack } from "@gluestack-ui/themed";
+
+export function SignIn(){
+    return(
+        <VStack flex={1} bg="$gray700"> </VStack>
+    )
+}
