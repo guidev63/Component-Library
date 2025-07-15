8488543fa30466de7fd2dc0d2690d0b4abfc75d5
Guilherme Augusto <guilhermedev23@gmail.com>
2025-07-15 15:56:34 -0300

    ok

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,26 @@
-import { VStack, Image } from "@gluestack-ui/themed";
+import { VStack, Image, Center } from "@gluestack-ui/themed";
 
 import BackgroundImage from "@assets/background.png"
-
+import Logo from "@assets/logo.svg"
+import { Text } from "react-native-svg";
 export function SignIn() {
     return (
         <VStack flex={1} bg="$gray700">
 
-        <Image
-        w="100%"
-        h={624}
-         source={BackgroundImage} 
-         defaultSource={BackgroundImage}
-         alt="Pessoa Treinando"
-         position="absolute"
-         />
+            <Image
+                w="100%"
+                h={624}
+                source={BackgroundImage}
+                defaultSource={BackgroundImage}
+                alt="Pessoa Treinando"
+                position="absolute"
+            />
+            <Center my="$24">
+                <Logo />
+                <Text color="$gray100" fontSize="$sm">
+                    Treine Sua Mente e o seu Corpo.
+                </Text>
+            </Center>
         </VStack>
     )
 }
