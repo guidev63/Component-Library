d2fc4525e670f8106c315b508f7aa395ed63c170
Guilherme Augusto <guilhermedev23@gmail.com>
2025-07-21 13:54:18 -0300

    ok

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,16 @@
 import { Input as GluestackInput, InputField } from "@gluestack-ui/themed"
+import { ComponentProps } from "react"
 
-export function Input() {
+
+type Props =  ComponentProps<typeof InputField>
+
+
+export function Input({...rest}: Props) {
     return (
-        <GluestackInput>
+        <GluestackInput bg="$gray700" h="$12" p="$3">
 
             <InputField 
+            {...rest}
             />
             
         </GluestackInput>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,7 +2,7 @@ import { VStack, Image, Text, Center, Heading } from "@gluestack-ui/themed";
 
 import BackgroundImage from "@assets/background.png"
 import Logo from "@assets/logo.svg"
-import  {Input} from "@components/Input"
+import { Input } from "@components/Input"
 export function SignIn() {
     return (
         <VStack flex={1} bg="$gray700">
@@ -15,16 +15,21 @@ export function SignIn() {
                 alt="Pessoa Treinando"
                 position="absolute"
             />
-            <Center my="$24">
-                <Logo />
-                <Text color="$gray100" fontSize="$sm">
-                    Treine Sua mente e o Seu Corpo.
-                </Text>
-            </Center>
-            <Center gap="$2">
-                <Heading color="$gray100"> Acesse A Conta </Heading>
-                <Input />
-            </Center>
+            <VStack flex={1} px="$10">
+                <Center my="$24">
+                    <Logo />
+                    <Text color="$gray100" fontSize="$sm">
+                        Treine Sua mente e o Seu Corpo.
+                    </Text>
+                </Center>
+                <Center gap="$2">
+                    <Heading color="$gray100"> Acesse A Conta </Heading>
+                    <Input placeholder="E-mail"
+                    />
+                    <Input placeholder="Senha"    
+                    />
+                </Center>
+            </VStack>
         </VStack>
     )
 }
